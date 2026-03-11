4938571080d09e484c65c1d101930ae9b98d5ef0
Vanina Maldonado <vanimaldo.vm@gmail.com>
2026-03-11 17:33:20 -0300

    Seagrego el codigo para el juego de preguntas

--- trivia.py
+++ trivia.py
@@ -7,3 +7,25 @@ pregunta1 = int(input("Cuant es la suma de 89 + 56?"))
 pregunta2 = input("Como se llama el artefacto que vuela para transportar gente?")
 pregunta3 = int(input("Cuantas patas tienen los insectos?"))
 pregunta4 = input("Cual es el color de la frutilla?")
+
+# Preguntas al usuario
+if pregunta1 == 145:
+    contador + 1 
+elif pregunta2 == "avión":
+    contador + 1
+elif pregunta3 == 6:
+    contador + 1 
+elif pregunta4 == "rojo" and "Rojo":
+    contador + 1
+else: 
+    print("Terminaron las preguntas")
+
+## Puntajes
+puntaje_total = 4
+
+if contador == puntaje_total:
+    print("Excelente!!! Puretasoo!!")
+elif contador >= puntaje_total:
+    print("Muy bien!")
+else:
+    print("Puede mejorar!")
