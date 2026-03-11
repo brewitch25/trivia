8d5fafbf8eec9f9aa78efba447877a86ae6b3460
Vanina Maldonado <vanimaldo.vm@gmail.com>
2026-03-11 17:23:12 -0300

    Se agregaron las preguntas

--- trivia.py
+++ trivia.py
@@ -1,2 +1,9 @@
 jugador = input("Ingrese su nombre: ")
 print(f"Bienvenido al juego de preguntas! {jugador}")
+
+contador = 0
+
+pregunta1 = int(input("Cuant es la suma de 89 + 56?"))
+pregunta2 = input("Como se llama el artefacto que vuela para transportar gente?")
+pregunta3 = int(input("Cuantas patas tienen los insectos?"))
+pregunta4 = input("Cual es el color de la frutilla?")
